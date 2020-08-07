7e21faee3faa04436bb6ca81319b45b74719da3c
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-08-07 09:06:07 -0700

    add first DB migration

--- Sneuss/Models/SneussContext.cs
+++ Sneuss/Models/SneussContext.cs
@@ -6,13 +6,8 @@ namespace Sneuss.Models
   {
     public DbSet<Engineer> Engineers { get; set; }
     public DbSet<Machine> Machines {get; set; }
-    public DbSet<EngineerMachine> EngineerMachines {get; set; }
+    public DbSet<EngineerMachine> EngineerMachine {get; set; }
     
     public SneussContext(DbContextOptions options) : base(options) { }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-      optionsBuilder.UseLazyLoadingProxies();
-    }
   }
 }
